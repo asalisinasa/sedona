4882146d8bfe72907bf597894c524f711f75063e
Alina <asalisinasa@gmail.com>
2015-11-11 17:04:48 +0300

    Завершение вёрстки личного проекта

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var link = document.querySelector(".search-push");
     var popup = document.querySelector(".search-form");
     var dateArrive = popup.querySelector("[name=date-arrive]");
+    var form = popup.querySelector("form");
 
     link.addEventListener("click", function(event) {
       event.preventDefault();
@@ -11,6 +12,16 @@ var link = document.querySelector(".search-push");
       if (event.keyCode === 27) {
         if (popup.classList.contains("search-form-show")) {
           popup.classList.remove("search-form-show");
+          popup.classList.remove("modal-error");
         }
       }
     });
+    /* где-то ошибка, не работает. при нажатии на кнопку при наличии пустых полей, просто перезагружается страница*/
+    form.addEventListener("submit", function(event) {
+        if (!date-arrive.value || !date-depart.value || !adult-numb.value) {
+          event.preventDefault();
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+        }
+      });
