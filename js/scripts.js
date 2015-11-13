e174711a1c7d4633f1ff9acdd68937792f145c2f
Alina <asalisinasa@gmail.com>
2015-11-13 23:13:48 +0300

    Сдача проекта

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
-var link = document.querySelector(".search-push");
+    var link = document.querySelector(".search-push");
     var popup = document.querySelector(".search-form");
-    var dateArrive = popup.querySelector("[name=date-arrive]");
-    var form = popup.querySelector("form");
+    if (link != null && popup != null) { //
+      var dateArrive = popup.querySelector("[name=date-arrive]");
+      var dateDepart = popup.querySelector("[name=date-depart]");
+      var adultNumb = popup.querySelector("[name=adult-numb]");
+      var form = popup.querySelector("form");
 
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.toggle("search-form-show");
-      dateArrive.focus()
-          });
-    window.addEventListener("keydown", function(event) {
-      if (event.keyCode === 27) {
-        if (popup.classList.contains("search-form-show")) {
-          popup.classList.remove("search-form-show");
-          popup.classList.remove("modal-error");
-        }
-      }
-    });
-    /* где-то ошибка, не работает. при нажатии на кнопку при наличии пустых полей, просто перезагружается страница*/
-    form.addEventListener("submit", function(event) {
-        if (!date-arrive.value || !date-depart.value || !adult-numb.value) {
+      link.addEventListener("click", function(event) {
           event.preventDefault();
+          popup.classList.toggle("search-form-show");
           popup.classList.remove("modal-error");
-          popup.offsetWidth = popup.offsetWidth;
-          popup.classList.add("modal-error");
-        }
+          dateArrive.focus()
       });
+      window.addEventListener("keydown", function(event) {
+          if (event.keyCode === 27) {
+              if (popup.classList.contains("search-form-show")) {
+                  popup.classList.remove("search-form-show");
+                  popup.classList.remove("modal-error");
+              }
+          }
+      });
+      form.addEventListener("submit", function(event) {
+          if (!dateArrive.value || !dateDepart.value || !adultNumb.value) {
+              event.preventDefault();
+              popup.classList.remove("modal-error");
+              popup.offsetWidth = popup.offsetWidth;
+              popup.classList.add("modal-error");
+          }
+      });
+    };
